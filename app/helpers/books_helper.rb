1c2f27b98f1262b07425373683aadbd07b086ea5
Kobito <kobito@orange.mykobito.net>
2011-07-18 01:58:38 +0900

    Make 2 helper methods.
    - delete_link
    - links_for

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,16 @@
+# -*- coding: utf-8 -*-
 module BooksHelper
+  def links_for(book)
+    links = []
+    links << link_to("修正", [ :edit, book ])
+    if book.checked_out?
+      links << link_to("返却", [ :check_in, book ],
+                       {:method => :put, :confirm => "返却しますか？"})
+    else
+      links << link_to("貸し出し", [ :check_out, book ],
+                       {:method => :put, :confirm => "貸し出しにしますか？"})
+    end
+    links << delete_link(book)
+    raw(links.join("／"))
+  end
 end
