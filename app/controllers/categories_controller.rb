925ed359f716fd5f2a425c44d692cb7f974c308a
hachiilcane <hachi.il.cane@gmail.com>
2011-11-12 01:18:38 +0900

    Added new actions of Category: new, edit, create, update, destroy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,37 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to :categories
+    else
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.attributes = params[:category]
+    if @category.save
+      redirect_to :categories
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to :categories
+  end
 end
