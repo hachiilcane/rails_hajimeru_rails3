e77d889e3c05824dc2c86a85b924d8a714f39d2a
hachiilcane <hachi.il.cane@gmail.com>
2011-11-13 22:48:17 +0900

    Added the uniqueness validation for Category model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,23 @@
+require 'nkf'
+
 class Category < ActiveRecord::Base
   has_many :books, :dependent => :nullify
-  validates :name, :presence => true
+  validates :name, :presence => true, :length => { :maximum => 10 }
+  validates :name, :uniqueness => { :case_sensitive => false }
   validates :color, :presence => true
+
+  before_validation :normalize_values
+
+  private
+
+  IDEOGRAPHIC_SPACE = [ 0x3000 ].pack("U")
+  WHITESPACES = "[\s#{IDEOGRAPHIC_SPACE}]"
+
+  def normalize_values
+    if name.present?
+      self.name = NKF.nkf("-WwZ", name)
+      self.name = name.sub(/^#{WHITESPACES}+/, "")
+      self.name = name.sub(/#{WHITESPACES}+$/, "")
+    end
+  end
 end
